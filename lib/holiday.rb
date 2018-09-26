0df5b1de1f760cd195f1415003840861a62a784e
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-26 15:52:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,6 +88,6 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.map do |holiday, supplies|
       holiday if supplies.include?("BBQ")
     end
-  end.compact
+  end.flatten.compact
 end
 binding.pry
